29a96576d95f18667dbcf9a22dac4f5eff931e61
Erik Bergkamp <eriking5@hotmail.com>
2021-07-19 12:12:33 -0300

    Criando classe posição

---
+++ xadrez/table/Position.cs
@@ -0,0 +1,20 @@
+﻿namespace table
+{
+    class Position
+    {
+        public int row { get; set; }
+        public int column { get; set; }
+
+        public Position(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public override string ToString()
+        {
+            return $"Posição: {row},{column} ";
+            
+        }
+    }
+}
